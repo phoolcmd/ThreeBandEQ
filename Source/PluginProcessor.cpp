85ed8b75576be0d92251c81190e4d1eaed4aa266
sentinelcmd <121768237+phoolcmd@users.noreply.github.com>
2023-01-27 11:32:34 -0700

    refactored cut filter creation

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -301,8 +301,7 @@ void updateCoefficients(Coefficients& old, const Coefficients& replacements)
 void ThreeBandEQAudioProcessor::updateLowCutFilters(const ChainSettings& chainSettings)
 {
 
-    auto lowCutCoefficients = juce::dsp::FilterDesign<float>::designIIRHighpassHighOrderButterworthMethod(chainSettings.lowCutFreq, getSampleRate(), 2 * (chainSettings.lowCutSlope + 1));
-
+    auto lowCutCoefficients = makeLowCutFilter(chainSettings, getSampleRate());
     auto& leftLowCut = leftChain.get<ChainPositions::LowCut>();
     auto& rightLowCut = rightChain.get<ChainPositions::LowCut>();
 
@@ -312,7 +311,7 @@ void ThreeBandEQAudioProcessor::updateLowCutFilters(const ChainSettings& chainSe
 
 void ThreeBandEQAudioProcessor::updateHighCutFilters(const ChainSettings& chainSettings)
 {
-    auto highCutCoefficients = juce::dsp::FilterDesign<float>::designIIRLowpassHighOrderButterworthMethod(chainSettings.highCutFreq, getSampleRate(), 2 * (chainSettings.highCutSlope + 1));
+    auto highCutCoefficients = makeHighCutFilter(chainSettings, getSampleRate());
 
     auto& leftHighCut = leftChain.get<ChainPositions::HighCut>();
     auto& rightHightCut = rightChain.get<ChainPositions::HighCut>();
